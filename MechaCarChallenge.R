4a7afba850960fdde4bb755f152a2624639ce800
Jeff Bailey <j.bailey8316@att.net>
2021-06-05 17:25:16 -0400

    updated MechaCraChallenge.R, added images and suspension_coil.csv

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,3 +13,19 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
 #summarize linear regression
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
              ground_clearance + AWD, data=mpg_data_df))
+
+# Deliverable 2
+
+#2 Import coil CSV file as dataframe
+coil_df <- read.csv(file = 'resources/Suspension_Coil.csv')
+
+#3 Create a total_summary df 
+tot_summary <- coil_df%>%data.frame()%>%summarize(Mean=mean(PSI),Median=median(PSI),
+                                                  Variance=var(PSI),SD=sd(PSI),)
+
+#4 Create a lot_summary df  
+lot_summary <- coil_df%>%group_by(Manufacturing_Lot)%>%summarize(Mean=mean(PSI),
+                                                                 Median=median(PSI),
+                                                                 Variance=var(PSI),
+                                                                 SD=sd(PSI), .groups = 'keep')
+
