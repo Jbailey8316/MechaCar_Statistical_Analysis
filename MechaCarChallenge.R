1daff94c3946b70f5b6e5c70cf80c3d8a9938def
Jeff Bailey <j.bailey8316@att.net>
2021-06-05 17:00:15 -0400

    upload deliverable1 files

---
+++ MechaCarChallenge.R
@@ -0,0 +1,15 @@
+#Deliverable 1
+
+#Load library 
+library(dplyr)
+
+#Import mpg csv file as a dataframe
+mpg_data_df <- read.csv("resources/MechaCar_mpg.csv")
+
+#Linear Regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+     ground_clearance + AWD, data=mpg_data_df)
+
+#summarize linear regression
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+             ground_clearance + AWD, data=mpg_data_df))
